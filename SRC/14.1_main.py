be5a0c7035e97b13e43b4b8539e8ccd105255de1
drauzer89 <drauzer89@gmail.com>
2025-07-11 00:36:40 +0300

    homework-requirements

--- SRC/14.1_main.py
+++ SRC/14.1_main.py
@@ -1,3 +1,23 @@
+class Product:
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+class Category:
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name: str, description: str, products: list):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(products)
+
+
+
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
